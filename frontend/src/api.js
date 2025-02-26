f6270750198b89a0bbc1f6a4bdbc8250fb021504
Ashish Raghuvanshi <thetypo36@gmail.com>
2025-02-26 15:39:46 +0530

    removed slash from api file in frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,17 +1,17 @@
 const API_URLS = {
-  PROD_BASE_URL: String(import.meta.env.VITE_PROD_BASE_URL),
-  LOGIN: `${String(import.meta.env.VITE_PROD_BASE_URL)}/${String(
+  PROD_BASE_URL: import.meta.env.VITE_PROD_BASE_URL, // No need for String()
+  LOGIN: `${import.meta.env.VITE_PROD_BASE_URL}${
     import.meta.env.VITE_LOGIN_PATIENT_ROUTE
-  )}`,
-  LOGOUT: `${String(import.meta.env.VITE_PROD_BASE_URL)}/${String(
+  }`,
+  LOGOUT: `${import.meta.env.VITE_PROD_BASE_URL}${
     import.meta.env.VITE_LOGOUT_PATIENT_ROUTE
-  )}`,
-  REGISTER: `${String(import.meta.env.VITE_PROD_BASE_URL)}/${String(
+  }`,
+  REGISTER: `${import.meta.env.VITE_PROD_BASE_URL}${
     import.meta.env.VITE_REGISTER_PATIENT_ROUTE
-  )}`,
-  GET_PATIENTS_TOKEN: `${String(import.meta.env.VITE_PROD_BASE_URL)}/${String(
+  }`,
+  GET_PATIENTS_TOKEN: `${import.meta.env.VITE_PROD_BASE_URL}${
     import.meta.env.VITE_GET_TOKEN_NO
-  )}`,
+  }`,
 };
 
 export { API_URLS };
