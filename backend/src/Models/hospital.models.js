c5b9336f32c768d9e798423c5a8663cf39848c64
Ashish Raghuvanshi <thetypo36@gmail.com>
2025-03-04 22:48:34 +0530

    improving aggregate pipeline to populate admin dashboard

--- backend/src/Models/hospital.models.js
+++ backend/src/Models/hospital.models.js
@@ -1,21 +1,30 @@
 import mongoose from "mongoose";
 
-const hospitalSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
+const hospitalSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
 
-  department: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Department",
-    required: true,
+    department: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Department",
+        required: true,
+      },
+    ],
+    doctor: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Doctor",
+        required: true,
+      },
+    ],
   },
-  doctor: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Doctor",
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export const Hospital = mongoose.model("Hospital", hospitalSchema);
